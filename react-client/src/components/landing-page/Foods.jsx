e535a6b347a7bba15ab2fc5abf977578efe83e21
Ose-Augustine <osebeckley@gmail.com>
2023-04-27 00:47:11 +0100

    Files in public folder are served in the root path.

--- react-client/src/components/landing-page/Foods.jsx
+++ react-client/src/components/landing-page/Foods.jsx
@@ -1,10 +1,12 @@
+// is going to dynamically display different images 4
+// at a time
 export default function FoodsSlide(){
     return (
-        <div className="grid grid-cols-4 gap-2 border-4 border-green-500 px-10">
-            <img  src="../../../public/images/joloff-rice-with-dodo.jpg" alt="Picture of jollof rice with fried plantain" />
-            <img  src="../../../public/images/egusi-and-eba.jpg" alt="Picture of egusi with eba" />
-            <img  src="../../../public/images/oha-soup.jpg" alt="Picture of oha soup" />
-            <img  src="../../../public/images/swallow-with-fufu.jpg" alt="Picture of a soup with fufu"/>
+        <div className="grid grid-cols-4 gap-2 border-4 border-green-500 px-10 mb-9">
+            <img  src="./images/joloff-rice-with-dodo.jpg" alt="Picture of jollof rice with fried plantain" />
+            <img  src="./images/egusi-and-eba.jpg" alt="Picture of egusi with eba" />
+            <img  src="./images/oha-soup.jpg" alt="Picture of oha soup" />
+            <img  src="./images/swallow-with-fufu.jpg" alt="Picture of a soup with fufu"/>
         </div>
     )
 }
