64acf8e4add5adffeab098ffa747f7dc68b754cc
Ose-Augustine <osebeckley@gmail.com>
2023-04-27 01:25:09 +0100

    Completed static food info.

--- react-client/src/App.jsx
+++ react-client/src/App.jsx
@@ -1,10 +1,12 @@
 import FoodsSlide from "./components/landing-page/Foods"
+import FoodInfo from "./components/landing-page/Info"
 import Hero from "./components/landing-page/hero"
 function App() {
   return (
     <>
       <Hero />
       <FoodsSlide />
+      <FoodInfo />
     </>
   )
 }
---
+++ react-client/src/components/landing-page/Info.jsx
@@ -0,0 +1,24 @@
+// Display a random food and button for more 
+// About the food
+export default function FoodInfo(){
+    return (
+        <div className="bg-gray-200  relative border-4 border-purple-500 h-screen flex">
+            
+            <div className="h-52 w-52 rounded-full bg-red-700 absolute right-36 top-6"></div>
+            <div className="absolute top-32 right-64">
+                <img className="h-96" src="./images/italian-seafood-pasta.jpg" alt="Picture of italian seafood pasta" />
+            </div>
+            <div className="border-4 border-yellow-800 absolute bottom-8 left-40 h-96 w-96 p-12 bg-white flex flex-col justify-evenly">
+                {/* header to change according to category of food displayed*/}
+                <header className="font-semibold text-4xl">Breakfast</header>
+                <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                    Velit beatae iure, vitae, nobis atque tempora, 
+                    eveniet mollitia et nesciunt odit.
+                    Blanditiis at temporibus autem labore obcaecati assumenda dignissimos nihil.
+                </p>
+                <button className="bg-red-500 text-white font-semibold rounded-lg self-start p-4">Learn more</button>
+            </div>
+        </div>
+    )
+}
