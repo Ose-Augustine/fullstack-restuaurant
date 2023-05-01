2f914d56792eb7faf15780f36df778afec4f2a31
Ose-Augustine <osebeckley@gmail.com>
2023-05-01 19:33:23 +0100

    Consume food images unsplash endpoint.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,19 @@
 require 'faker' 
+require 'httparty'
+
+response = HTTParty.get("https://api.unsplash.com/photos/random?query=foods&client_id=#{ENV["UNSPLASH_FOODS_ACCESS_KEY"]}&count=20")
+pictures_urls = []
+response.each do |response|
+    pictures_urls.push(response["urls"]["small"])
+end 
 
 # Generate food data 
-
-2.times do 
+20.times do |n|
     name = Faker::Food.dish 
     info = Faker::Lorem.sentence(word_count:13)
-    image = Faker::Avatar.image(size: "50x50", format:"jpg")
+    image = pictures_urls[n]
     category = Faker::Food.ethnic_category
+    n += 1
     Food.create(
         name: name,
         about: info,
