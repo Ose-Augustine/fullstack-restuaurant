1e593d069cffb820f8bea6d17782044e980659b1
Ose-Augustine <osebeckley@gmail.com>
2023-05-11 17:02:37 +0100

    Handle json type of form data.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,7 @@
+require 'httparty' 
 class OrdersController < ApplicationController
+    respond_to :json 
+
     def new
         food = Food.find_by(name: params[:food])
         drink = Drink.find_by(name: params[:drink])
@@ -6,6 +9,7 @@ class OrdersController < ApplicationController
             food_id: food[:id],  
             drink_id: drink[:id]
         )
+        return {success: true, message:"Order created successfully"}.to_json 
     end
 
     private
