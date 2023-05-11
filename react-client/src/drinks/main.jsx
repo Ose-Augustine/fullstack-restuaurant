3dc5d660d56c3a5a552a38722ff66a4066ade353
Ose-Augustine <osebeckley@gmail.com>
2023-05-11 17:06:58 +0100

    Main render for drinks.

---
+++ react-client/src/drinks/main.jsx
@@ -0,0 +1,18 @@
+import DrinksIndex from "./index"; 
+import getAPIData from "../api-connection";
+import { useEffect, useState } from "react";
+
+export default function DrinksMainComp (){
+    const [drinks, setDrinks] = useState([]);
+
+    const API_URL = "http://localhost:3000/drinks"; 
+    useEffect(() => {
+        getAPIData(API_URL).then(items => setDrinks(items));
+        },[])
+    return (
+        <>
+            <div>this is the drinks main page right</div>
+            <DrinksIndex drinks={drinks} />
+        </>
+    )
+}
